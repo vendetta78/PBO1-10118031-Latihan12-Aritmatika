f8f22b138cab8685c171b46944135198059d138d
vendetta78 <55823091+vendetta78@users.noreply.github.com>
2019-09-26 12:04:34 +0700

    Add files via upload

---
+++ PBO1-10118031-Latihan12-Aritmatika/src/pbo1/pkg10118031/latihan12/aritmatika/PBO110118031Latihan12Aritmatika.java
@@ -0,0 +1,32 @@
+package pbo1.pkg10118031.latihan12.aritmatika;
+
+/**
+ *
+ * @author Vendetta
+ * NAMA              : Rizki Aria Putra
+ * KELAS             : PBO1
+ * NIM               : 10118031
+ * Deskripsi Program : Program ini berisi program untuk 
+ *                     operator Aritmatika
+ */
+public class PBO110118031Latihan12Aritmatika {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a =" + (b / a));
+        System.out.println("b % a =" + (b % a));
+        System.out.println("c % a =" + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+
+    }
+
+}
